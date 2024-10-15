853bfbb40e97d1bc0f1f19f0dbb1dc183e1ebbdb
prajeshElEvEn <prajesh.eleven118@gmail.com>
2024-10-15 10:40:03 +0530

    added feed posts styling

--- app/controllers/feed_controller.rb
+++ app/controllers/feed_controller.rb
@@ -1,6 +1,7 @@
 class FeedController < ApplicationController
   before_action :authenticate_user!
+
   def index
-  @posts = Post.all.includes(:user)
+    @posts = Post.order(created_at: :desc).includes(:user)
   end
 end
